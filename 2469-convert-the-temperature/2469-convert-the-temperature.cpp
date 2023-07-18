d6966385dd1433de2c9324da0270d49b686d44fe
SHAURYA SRIVASTAVA <30shaurya@gmail.com>
2023-07-18 18:40:13 +0530

    Time: 4 ms (10.86%), Space: 6 MB (47.30%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        double kelvin = celsius + 273.15;
+        double fahrenheit = celsius * 1.80 + 32;
+        vector<double> ans;
+        
+        ans.push_back(kelvin);
+        ans.push_back(fahrenheit);
+        return ans;
+    }
+};
