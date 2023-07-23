608640d6b820a6533afe4c847ba877f9817dd99e
SHAURYA SRIVASTAVA <30shaurya@gmail.com>
2023-07-23 20:02:13 +0530

    Time: 4 ms (37.33%), Space: 6.3 MB (32.46%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int balance = 0;
+        int ans = 0;
+        for(int i = 0; i< s.size(); i++){
+            if(s[i] == 'L'){
+                balance++;
+            }
+            else{
+                balance--;
+            }
+            if(balance == 0){
+                ans++;
+            }
+        }
+        return ans;
+        
+    }
+};
