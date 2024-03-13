f57bac3c1d29c2f615cc1d5b62821856e1056d88
SHAURYA SRIVASTAVA <30shaurya@gmail.com>
2024-03-13 11:38:03 +0530

    Time: 16 ms (37.85%), Space: 20.9 MB (37.05%) - LeetHub

---
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestMountain(vector<int>& A) {
+        int N = A.size();
+        int ans = 0, base = 0;
+        while (base < N) {
+            int end = base;
+            // if base is a left-boundary
+            if (end + 1 < N && A[end] < A[end + 1]) {
+                // set end to the peak of this potential mountain
+                while (end + 1 < N && A[end] < A[end + 1]) end++;
+
+                // if end is really a peak..
+                if (end + 1 < N && A[end] > A[end + 1]) {
+                    // set end to the right-boundary of the mountain
+                    while (end + 1 < N && A[end] > A[end + 1]) end++;
+                    // record candidate answer
+                    ans = max(ans, end - base + 1);
+                }
+            }
+
+            base = max(end, base + 1);
+        }
+
+        return ans;
+    }
+};
