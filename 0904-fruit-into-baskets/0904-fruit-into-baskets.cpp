cd2bc3a89af20fb31cf7b94380b052e0f79611fa
SHAURYA SRIVASTAVA <30shaurya@gmail.com>
2024-04-04 19:06:15 +0530

    Time: 128 ms (37.97%), Space: 77.6 MB (8.34%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
+        
+        // most optimal solution
+        
         int left = 0, right = 0, maxLen = 0, n = fruits.size();
         map<int, int> mp;
         
@@ -8,22 +11,18 @@ public:
             mp[fruits[right]]++;
             
             if(mp.size() > 2){
-                while(mp.size() > 2){
-                    mp[fruits[left]]--;
-                    if(mp[fruits[left]] == 0){
-                        mp.erase(fruits[left]);
-                    }
-                    left++;
-                }
+                mp[fruits[left]]--;
+                
+                if(mp[fruits[left]] == 0) mp.erase(fruits[left]);
+                
+                left++;
             }
             
-            if(mp.size() <= 2){
-                maxLen = max(maxLen, right - left +1);
+            else{
+                maxLen = max(maxLen, right-left+1);
             }
             right++;
         }
-        
         return maxLen;
-        
     }
 };
