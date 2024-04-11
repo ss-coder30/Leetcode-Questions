cfbf9506a9be2d12bf14b24fe54d9bc837e61b9e
SHAURYA SRIVASTAVA <30shaurya@gmail.com>
2024-04-11 11:56:03 +0530

    Time: 24 ms (79.05%), Space: 30.3 MB (14.71%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,22 +1,50 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        
-        unordered_map<string, vector<string>> mp;
-        int n = strs.size();
-        string temp;
-        vector<vector<string>> ans;
-        
-        for(int i = 0; i<n; i++){
-            temp = strs[i];
-            sort(strs[i].begin(), strs[i].end());
-            mp[strs[i]].push_back(temp);
+        vector<vector<string>> groups;
+        std::unordered_map<string, vector<string>> hash_table;
+
+        // Time: O(n * m)
+        // Space: O(n * m)
+        for (auto& str : strs)
+        {
+            string signature;
+            vector<int> signature_array(26, 0);
+
+            // Count each letter in string.
+            for (auto& letter : str)
+            {
+                int letter_i = letter - 'a';
+                signature_array[letter_i]++;
+            }
+
+            // Create signature from letter count of string.
+            for (auto& letter_count : signature_array)
+            {
+                signature.push_back(letter_count + '0');
+            }
+
+            // If signature already exists, add string to group.
+            if (hash_table.contains(signature))
+            {
+                hash_table[signature].emplace_back(str);
+            }
+            // Otherwise, create new group with string.
+            else
+            {
+                vector<string> group;
+                group.emplace_back(str);
+                hash_table.emplace(signature, group);
+            }
         }
-        
-        for(auto it: mp){
-            ans.push_back(it.second);
+
+        // Time: O(n)
+        // Space: O(n)
+        for (auto& [signature, group] : hash_table)
+        {
+            groups.emplace_back(group);
         }
-        
-        return ans;
+
+        return groups;
     }
 };
