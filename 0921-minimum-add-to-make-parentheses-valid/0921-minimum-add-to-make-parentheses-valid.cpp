a9eff14dee726e3f53ce6668d0288cfd9a1ef183
SHAURYA SRIVASTAVA <30shaurya@gmail.com>
2024-04-28 14:41:42 +0530

    Time: 3 ms (43.52%), Space: 7.5 MB (69.62%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        
+        int count = 0;
+        int ans = 0;
+        
+        for(int i = 0; i<s.size(); i++){
+            
+            if(s[i] == '(') count++;
+            
+            else {
+                if(count == 0) ans++;
+                else count--;
+            }
+        }
+        
+        ans += count;
+        
+        return ans;
+    }
+};
