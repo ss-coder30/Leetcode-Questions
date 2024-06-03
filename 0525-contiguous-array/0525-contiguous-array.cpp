efaad0c2a59d8da6fa1626596cb87bfc93451a23
SHAURYA SRIVASTAVA <30shaurya@gmail.com>
2024-06-03 13:11:00 +0530

    Time: 80 ms (76.41%), Space: 87.6 MB (77.67%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        
+        unordered_map<int, int> map;
+        map[0] = -1;
+        
+        int maxLen = 0, count = 0;
+        
+        for(int i = 0; i<nums.size(); i++){
+            count += (nums[i] == 1 ? 1 : -1);
+            
+            if(map.count(count)){
+                maxLen = max(maxLen, i - map[count]);
+            }
+            else{
+                map[count] = i;
+            }
+        }
+        
+        return maxLen;
+    }
+};
