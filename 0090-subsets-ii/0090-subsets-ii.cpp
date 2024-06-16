49aec4486f87dca31548b96f5b87644219329c52
SHAURYA SRIVASTAVA <30shaurya@gmail.com>
2024-06-17 00:54:30 +0530

    Time: 4 ms (48.90%), Space: 8.8 MB (57.61%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    void subsets(int index, vector<int>& nums, vector<int>& ds, vector<vector<int>>& ans){
+        ans.push_back(ds);
+        
+        for(int i = index; i<nums.size(); i++){
+            
+            if(i != index && nums[i] == nums[i-1]) continue;
+            
+            ds.push_back(nums[i]);
+            subsets(i+1, nums, ds, ans);
+            ds.pop_back();
+        }
+    }
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        
+        vector<vector<int>> ans;
+        vector<int> ds;
+        sort(nums.begin(), nums.end());
+        
+        subsets(0, nums, ds, ans);
+        
+        return ans;
+    }
+};
