11efb58710a724483284af46f2dcd660c380fd9d
SHAURYA SRIVASTAVA <30shaurya@gmail.com>
2024-08-12 17:47:02 +0530

    Time: 27 ms (51.98%), Space: 24.8 MB (39.10%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -1,7 +1,7 @@
 class KthLargest {
     
-private:
-    priority_queue<int, vector<int>, greater<int>> pq;
+private: 
+    priority_queue<int, vector<int>, greater<int>>pq;
     int size;
     
 public:
@@ -15,10 +15,9 @@ public:
     }
     
     int add(int val) {
-        
         pq.push(val);
-        if(pq.size() > size) pq.pop();
         
+        if(pq.size() > size) pq.pop();
         return pq.top();
     }
 };
