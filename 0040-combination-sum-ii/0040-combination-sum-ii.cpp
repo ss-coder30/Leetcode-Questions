50977f91184e49c8192908ab5afb5c0dee2813d3
SHAURYA SRIVASTAVA <30shaurya@gmail.com>
2024-08-14 00:50:28 +0530

    Time: 7 ms (34.54%), Space: 12.9 MB (38.57%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,29 +1,42 @@
 class Solution {
-public:
     
-    void findCombinations(int index, int target, vector<int>& ds, vector<int>& arr, vector<vector<int>>& ans){
+private:
+    void findCombinations(int n, int index, int target, vector<int>& ds, vector<vector<int>>& ans, vector<int>& a){
+        
         // base case
         if(target == 0){
             ans.push_back(ds);
             return;
         }
         
-        for(int i = index; i < arr.size(); i++){
-            if(i > index && arr[i] == arr[i-1]) continue; // skip duplicates
-            if(arr[i] > target) break; // no need to proceed if the current element is greater than the remaining target
+        for(int i = index; i<n; i++){
             
-            ds.push_back(arr[i]);
-            findCombinations(i + 1, target - arr[i], ds, arr, ans); // move to next index for each combination
-            ds.pop_back(); // backtrack
+            // base case
+            // consecutive duplicates
+            if(i > index && a[i] == a[i-1]) continue;
+            
+            // if a[i] > target
+            if(a[i] > target) break;
+            
+            //pick
+            ds.push_back(a[i]);
+            findCombinations(n, i+1, target-a[i], ds, ans, a);
+            
+            //backtrack
+            ds.pop_back();
         }
     }
     
+public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        
+        int n = candidates.size();
         vector<int> ds;
         vector<vector<int>> ans;
         
-        sort(candidates.begin(), candidates.end()); // sort the candidates to handle duplicates easily
-        findCombinations(0, target, ds, candidates, ans);
+        sort(candidates.begin(), candidates.end());
+        
+        findCombinations(n, 0, target, ds, ans, candidates);
         
         return ans;
     }
