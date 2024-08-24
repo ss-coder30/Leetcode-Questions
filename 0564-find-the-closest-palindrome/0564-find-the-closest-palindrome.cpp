2a511163e431e68d5478e0f21b81896aff462f19
SHAURYA SRIVASTAVA <30shaurya@gmail.com>
2024-08-24 21:35:21 +0530

    Time: 3 ms (45.31%), Space: 8.1 MB (92.96%) - LeetHub

--- 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.cpp
+++ 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.cpp
@@ -1,57 +1,55 @@
 class Solution {
 public:
-    long long int reverseVal(long long int val) {
-        long long int z = val;
-        while (z != 0) {
-            val = val * 10 + z % 10;
-            z /= 10;
+    long long int evenPalindrome(long long int val) {
+        long long int temp = val;
+        while (temp != 0) {
+            val = val * 10 + temp % 10;
+            temp /= 10;
         }
         return val;
     }
 
-    long long int reverseVal2(long long int val, long long int k) {
-        while (k != 0) {
-            val = val * 10 + k % 10;
-            k /= 10;
+    long long int oddPalindrome(long long int val, long long int temp) {
+        while (temp != 0) {
+            val = val * 10 + temp % 10;
+            temp /= 10;
         }
         return val;
     }
 
-    void minDiff(long long int org, long long int& val, long long& ma,
+    void updateMinDiff(long long int org, long long int& val, long long& minDiff,
                  long long int& ans) {
-        if (abs(org - val) < ma && abs(org - val) != 0) {
+        if (abs(org - val) < minDiff && abs(org - val) != 0) {
             ans = val;
-            ma = abs(org - val);
-        } else if (abs(org - val) == ma)
+            minDiff = abs(org - val);
+        } else if (abs(org - val) == minDiff)
             ans = min(ans, val);
     }
+    
     string nearestPalindromic(string s) {
         long long int n = s.size();
         long long int original = stoll(s);
 
-        long long int z = 0,k=0;
-        for (int i = 0; i <=n / 2; i++) {
-            z = z * 10 + s[i] - '0';
-            if(i<n/2) k=k*10+s[i]-'0';
+        long long int oddHalf = 0, evenHalf = 0;
+        for (int i = 0; i <= n / 2; i++) {
+            oddHalf = oddHalf * 10 + s[i] - '0';
+            if (i < n / 2) evenHalf = evenHalf * 10 + s[i] - '0';
         }
-        long long int ma = INT_MAX;
-        long long int ans = INT_MAX;
+        long long int minDiff = LLONG_MAX;
+        long long int ans = LLONG_MAX;
         
-        long long int first =
-            n % 2 == 0 ? reverseVal(k) : reverseVal2(z, z / 10);
-        minDiff(original, first, ma, ans);
-        long long int second =
-            n % 2 == 0 ? reverseVal(k - 1) : reverseVal2(z - 1, (z - 1) / 10);
-        minDiff(original, second, ma, ans);
-        long long int third =
-            n % 2 == 0 ? reverseVal(k + 1) : reverseVal2(z + 1, (z + 1) / 10);
-        minDiff(original, third, ma, ans);
+        long long int first = n % 2 == 0 ? evenPalindrome(evenHalf) : oddPalindrome(oddHalf, oddHalf / 10);
+        updateMinDiff(original, first, minDiff, ans);
+        long long int second = n % 2 == 0 ? evenPalindrome(evenHalf - 1) : oddPalindrome(oddHalf - 1, (oddHalf - 1) / 10);
+        updateMinDiff(original, second, minDiff, ans);
+        long long int third = n % 2 == 0 ? evenPalindrome(evenHalf + 1) : oddPalindrome(oddHalf + 1, (oddHalf + 1) / 10);
+        updateMinDiff(original, third, minDiff, ans);
 
         long long int fourth = pow(10, n - 1) - 1;
-        minDiff(original, fourth, ma, ans);
+        updateMinDiff(original, fourth, minDiff, ans);
 
         long long int fifth = pow(10, n) + 1;
-        minDiff(original, fifth, ma, ans);
+        updateMinDiff(original, fifth, minDiff, ans);
 
         return to_string(ans);
     }
